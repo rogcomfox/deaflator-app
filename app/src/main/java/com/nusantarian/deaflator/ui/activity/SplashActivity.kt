ef90175905be0a40a1ba20687dbf7afb25cbb1fa
rogcomfox <joeg.11021996@gmail.com>
2021-03-23 06:30:50 +0700

    finish layout & add finishAffinity after go to MainActivity

--- app/src/main/java/com/nusantarian/deaflator/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/nusantarian/deaflator/ui/activity/SplashActivity.kt
@@ -1,6 +1,9 @@
-package com.nusantarian.deaflator
+package com.nusantarian.deaflator.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.nusantarian.deaflator.databinding.ActivitySplashBinding
 
@@ -12,5 +15,10 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finishAffinity()
+        }, 2000)
     }
 }
