fb5e9239b4220ee2dba67b04eea58bf38ad874a2
rogcomfox <joeg.11021996@gmail.com>
2021-03-30 07:23:29 +0700

    add check permission algorithm

--- app/src/main/java/com/nusantarian/deaflator/ui/activity/MainActivity.kt
+++ app/src/main/java/com/nusantarian/deaflator/ui/activity/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.nusantarian.deaflator.ui.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.nusantarian.deaflator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+            checkPermission()
+    }
+
+    private fun checkPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                listOf(Manifest.permission.RECORD_AUDIO).toTypedArray(),
+                RecordAudioRequestCode)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == RecordAudioRequestCode && grantResults.isNotEmpty())
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        const val RecordAudioRequestCode = 1
     }
 }
