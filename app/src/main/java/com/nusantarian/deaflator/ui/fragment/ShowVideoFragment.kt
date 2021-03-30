df0577ebc38077cbcdd0ac8cf84e3424b85fe044
rogcomfox <joeg.11021996@gmail.com>
2021-03-30 07:24:06 +0700

    success show dummy video

---
+++ app/src/main/java/com/nusantarian/deaflator/ui/fragment/ShowVideoFragment.kt
@@ -0,0 +1,37 @@
+package com.nusantarian.deaflator.ui.fragment
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.MediaController
+import androidx.fragment.app.Fragment
+import com.nusantarian.deaflator.R
+import com.nusantarian.deaflator.databinding.FragmentShowVideoBinding
+
+class ShowVideoFragment : Fragment() {
+
+    private var _binding: FragmentShowVideoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentShowVideoBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    //Todo: This is just dummy data, need to change
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val videoPath = "android.resource://" + requireActivity().packageName + "/" + R.raw.video
+        binding.vvSignData.setVideoURI(Uri.parse(videoPath))
+
+        val mediaController = MediaController(activity)
+        binding.vvSignData.setMediaController(mediaController)
+        binding.vvSignData.start()
+    }
+}
