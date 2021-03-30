d90c1d6d4dfdb571ba06fdd16ce747c9c394d982
rogcomfox <joeg.11021996@gmail.com>
2021-03-30 07:25:22 +0700

    success speech to sign language algorithm, need to add backend

--- app/src/main/java/com/nusantarian/deaflator/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/nusantarian/deaflator/ui/fragment/MainFragment.kt
@@ -1,7 +1,14 @@
 package com.nusantarian.deaflator.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
@@ -12,11 +19,14 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.nusantarian.deaflator.R
 import com.nusantarian.deaflator.databinding.FragmentMainBinding
+import java.util.*
+
 
 class MainFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private lateinit var speech: SpeechRecognizer
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -25,21 +35,102 @@ class MainFragment : Fragment(), View.OnClickListener {
         // Inflate the layout for this fragment
         _binding = FragmentMainBinding.inflate(inflater, container, false)
 
+        speechToText()
+
         binding.icMore.setOnClickListener(this)
+        binding.tvData.setOnClickListener(this)
+        binding.tvData1.setOnClickListener(this)
+        binding.btnProcess.setOnClickListener {
+            binding.tvSignOutput.visibility = View.GONE
+            binding.tvData.visibility = View.VISIBLE
+        }
 
         return binding.root
+    }
+
+    //convert speech to text
+    private fun speechToText() {
+        speech = SpeechRecognizer.createSpeechRecognizer(activity)
+        val speechIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+        speech.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(params: Bundle?) {
+            }
+
+            override fun onBeginningOfSpeech() {
+                binding.tvResultSpeech.text = ""
+                binding.tvResultSpeech.hint = "Listening....."
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+            }
+
+            override fun onBufferReceived(buffer: ByteArray?) {
+            }
+
+            override fun onEndOfSpeech() {
+            }
+
+            override fun onError(error: Int) {
+            }
+
+            override fun onResults(results: Bundle) {
+                binding.imgRecord.setImageResource(R.drawable.ic_mic_black)
+                val data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                binding.tvResultSpeech.text = data?.get(0)
+                binding.tvData1.visibility = View.VISIBLE
+                binding.tvSignOutput1.visibility = View.GONE
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle?) {
+            }
+        })
+
+        binding.imgRecord.setOnTouchListener { view, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_UP) {
+                speech.stopListening()
+            }
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                binding.imgRecord.setImageResource(R.drawable.ic_mic_red)
+                speech.startListening(speechIntent)
+            }
+            false
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initDropdownMenu()
+        changeButtonState()
+    }
+
+    private fun changeButtonState() {
+        binding.etTextInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                binding.btnProcess.isEnabled = s.isNotEmpty()
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                binding.btnProcess.isEnabled = s.isNotEmpty()
+            }
+
+        })
     }
 
     private fun initDropdownMenu() {
         val items = listOf(getString(R.string.text_item_1), getString(R.string.text_item_2))
         val adapter = ArrayAdapter(requireContext(), R.layout.dropdown_translation_item, items)
         (binding.tilDropdown.editText as? AutoCompleteTextView)?.setAdapter(adapter)
-        (binding.tilDropdown.editText as? AutoCompleteTextView)?.setOnItemClickListener { parent, view, position, id ->
+        (binding.tilDropdown.editText as? AutoCompleteTextView)?.setOnItemClickListener { _, _, position, _ ->
 
             if (position == 0) {
                 binding.tvChooseMode.visibility = View.GONE
@@ -56,6 +147,12 @@ class MainFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View) {
         when (v.id) {
             R.id.ic_more -> raisePopup()
+            R.id.tv_data ->
+                findNavController()
+                        .navigate(MainFragmentDirections.actionMainFragmentToShowVideoFragment())
+            R.id.tv_data_1 ->
+                findNavController()
+                        .navigate(MainFragmentDirections.actionMainFragmentToShowVideoFragment())
         }
     }
 
@@ -85,5 +182,10 @@ class MainFragment : Fragment(), View.OnClickListener {
             }
         }
         popup.show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        speech.destroy()
     }
 }
